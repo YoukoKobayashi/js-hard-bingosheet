b4b14f1f0f282b7ed70e1c77ab3e0a60035f6b91
YoukoKobayashi <86542067+YoukoKobayashi@users.noreply.github.com>
2022-11-08 21:40:07 +0900

    Add files via upload
    通常問題終了

--- main.js
+++ main.js
@@ -1,82 +1,122 @@
-const arrSt = ['B', 'I', 'N', 'G', 'o'];
-let arrB = Array(5).fill();
-let b = 0;
-while (b<5) {
-    let n1 = Math.floor(Math.random() * 15) + 1;
-    arrB.push(n1);
-    if (arrB.includes(n1) === true) {
-        arrB.pop();
-    }
-    b += 1;
-}
-let arrI = Array(5).fill();
-let i = 0;
-while (i< 5) {
-    let n2 = Math.floor(Math.random() * (30-16+1)) + 16;
-    arrI.push(n2);
-    if (arrI.includes(n2) === true) {
-        arrI.pop();
-    }
-    i += 1;
-}
-let arrN = Array(5).fill();
-let n = 0;
-while (n < 5) {
-    let n3 = Math.floor(Math.random() * 45-31+1) + 31;
-    arrN.push(n3);
-    if (arrN.includes(n3) === true) {
-        arrN.pop();
-    }
-    n += 1;
-}
-arrN[2] = 'free';
-let arrG = Array(5).fill();
-let g = 0;
-while (g < 5) {
-    let n4 =
-        Math.floor(
-            Math.random() * (60 - 46 + 1)
-        ) + 46;
-    arrI.push(n4);
-    if (arrG.includes(n4) === true) {
-        arrG.pop();
-    }
-    g += 1;
-}
-let arrO = Array(5).fill();
-let o = 0;
-while (o < 5) {
-    let n5 =
-        Math.floor(
-            Math.random() * (75 - 61 + 1)
-        ) + 61;
-    arrO.push(n5);
-    if (arrO.includes(n5) === true) {
-        arrO.pop();
-    }
-    o += 1;
-}
+// const arrB = [];
+// let b = 0;
+// while (b < 5) {
+//     let n1 = Math.floor(Math.random() * 15) + 1;
+//     if (arrB.includes(n1) === false) {
+//         arrB.push(n1);
+//         b += 1;
+//     }
+// }
+// console.log(arrB);
 
-let arrNum = [];
-arrNum.push(arrB);
-arrNum.push(arrI);
-arrNum.push(arrN);
-arrNum.push(arrG);
-arrNum.push(arrO);
+// const arrI = [];
+// let i = 0;
+// while (i < 5) {
+//     let n2 =
+//         Math.floor(
+//             Math.random() * (30 - 16 + 1)
+//         ) + 16;
+//     if (arrI.includes(n2) === false) {
+//         arrI.push(n2);
+//         i += 1;
+//     }
+// }
+// const arrN = [];
+// let n = 0;
+// while (n < 5) {
+//     let n3 =
+//         Math.floor(
+//             Math.random() * (45 - 31 + 1)
+//         ) + 31;
+//     if (arrN.includes(n3) === false) {
+//         arrN.push(n3);
+//         n += 1;
+//     }
+// }
+// arrN[2] = "free";
+// const arrG = [];
+// let g = 0;
+// while (g < 5) {
+//     let n4 =
+//         Math.floor(
+//             Math.random() * (60 - 46 + 1)
+//         ) + 46;
+//     if (arrG.includes(n4) === false) {
+//         arrG.push(n4);
+//         g += 1;
+//     }
+// }
+// const arrO = [];
+// let o = 0;
+// while (o < 5) {
+//     let n5 =
+//         Math.floor(
+//             Math.random() * (75 - 61 + 1)
+//         ) + 61;
+//     if (arrO.includes(n5) === false) {
+//         arrO.push(n5);
+//         o += 1;
+//     }
+// }
+
+const arrTtl = ["B", "I", "N", "G", "O"];
+
+const arrNum = [];
+for (let a = 1; a <= 5; a++) {
+    const arr = [];
+    let i = 0;
+    while (i < 5) {
+        let n =
+            Math.floor(
+                Math.random() *
+                    (15 * a - 15 * (a - 1))
+            ) +
+            15 * (a - 1) +
+            1;
+        if (arr.includes(n) === false) {
+            arr.push(n);
+            i += 1;
+        }
+    }
+    arrNum.push(arr);
+}
+arrNum[2][2] = "free";
+//console.log(arrNum);
 
 const view = document.getElementById("view");
-const tr1 = document.createElement('tr');
-for (let index = 0; index < arrSt.length; index++){
-    tr1.appendChild(arrSt[index]);
+const tr = document.createElement("tr");
+view.appendChild(tr);
+for (let t = 0; t < arrTtl.length; t++) {
+    const th = document.createElement("th");
+    th.textContent = arrTtl[t];
+    view.appendChild(th);
 }
-
-for (let r = 0; r < 5; r++){
-    let tr = document.createElement('tr');
-    tr.textContent=
-    for (let d = 0; d < 5; d++){
-        let td = document.createElement('td');
-        td[r] = arrNum[r][d];
-        
+for (let r = 0; r < 5; r++) {
+    const tr = document.createElement("tr");
+    view.appendChild(tr);
+    for (let c = 0; c < 5; c++) {
+        const td = document.createElement("td");
+        td.textContent = arrNum[c][r];
+        view.appendChild(td);
     }
-
 }
+
+//challenge
+const hitNum = document.getElementById("hitNum");
+hitNum.addEventListener("click", () => {
+    const selectNum =
+        Math.floor(Math.random() * 75) + 1;
+    alert(`${selectNum}です`);
+
+    const tdd =
+        document.getElementsByTagName("td");
+    let i;
+    Array.from(tdd).forEach((_, i) => {
+        if (tdd[i].textContent === selectNum) {
+            tdd[i].setAttribute(
+                "class",
+                "hit-num"
+            );
+        }
+    });
+});
